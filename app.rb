c8eb10e099b8ce24db490e7df5eb2340930be231
ixellsony <88244854+ixellsony@users.noreply.github.com>
2024-09-27 00:36:21 +0200

    Add files via upload

--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
 require 'sinatra'
+require 'sqlite3'
 
+# Crée une méthode pour accéder à la base de données
+def db_connection
+  db = SQLite3::Database.new "test.db"
+  db.results_as_hash = true # Retourne les résultats sous forme de hash
+  db
+end
+
+# Crée la table si elle n'existe pas déjà
+db_connection.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS restaurants (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  );
+SQL
+
+# Route principale
 get '/' do
+  @restaurants = db_connection.execute("SELECT name FROM restaurants")
   erb :index
 end
 
+# Route /about
 get '/about' do
   erb :about
 end
