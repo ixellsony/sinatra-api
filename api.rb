9bfc5c2619194950792900fb9bd143a464d436ec
ixellsony <88244854+ixellsony@users.noreply.github.com>
2024-09-28 16:37:25 +0200

    Add files via upload

--- api.rb
+++ api.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 require 'sqlite3'
 require 'rack/cors'
 require 'jwt'
@@ -7,7 +8,7 @@ require 'bcrypt'
 use Rack::Cors do
   allow do
     origins '*' 
-    resource '*', headers: :any, methods: [:get, :post, :options]
+    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
   end
 end
 
@@ -23,7 +24,7 @@ dbc.execute <<-SQL
     name TEXT
   );
 
-    CREATE TABLE IF NOT EXISTS users (
+  CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
     password_hash TEXT
@@ -49,28 +50,36 @@ end
 def authenticate!
   token = request.env['HTTP_AUTHORIZATION']&.split(' ')&.last
   user_id = verify_token(token)
-  halt 401, "Unauthorized" unless user_id
+  halt 401, { message: "Unauthorized" }.to_json unless user_id
   user_id
 end
 
+# Filtre avant chaque requête pour définir le type de contenu
+before do
+  content_type :json
+end
+
+# Enregistrement d'un nouvel utilisateur
 post '/register' do
   username = params[:username]
   password = params[:password]
   
   if username.nil? || password.nil?
-    return "Username and password are required"
+    halt 400, { message: "Username and password are required" }.to_json
   end
   
   password_hash = BCrypt::Password.create(password)
   
   begin
     dbc.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", [username, password_hash])
-    "User registered successfully"
+    status 201
+    { message: "User registered successfully" }.to_json
   rescue SQLite3::ConstraintException
-    "Username already exists"
+    halt 409, { message: "Username already exists" }.to_json
   end
 end
 
+# Connexion utilisateur
 post '/login' do
   username = params[:username]
   password = params[:password]
@@ -81,58 +90,78 @@ post '/login' do
     token = create_token(user['id'])
     { token: token }.to_json
   else
-    "Invalid username or password"
+    halt 401, { message: "Invalid username or password" }.to_json
   end
 end
 
+# Récupération de la liste des restaurants
 get '/restaurants' do
-  authenticate!
-  @restaurants = dbc.execute("SELECT name FROM restaurants")
-  if @restaurants.any?
-    erb :restaurants
-  else
-    "Aucun restaurant"
-  end
+  restaurants = dbc.execute("SELECT * FROM restaurants")
+  { restaurants: restaurants }.to_json
 end
 
+# Ajout d'un nouveau restaurant
 post '/restaurant' do
   authenticate!
   name = params[:name]
+  
+  if name.nil? || name.empty?
+    halt 400, { message: "Name is required" }.to_json
+  end
+
   dbc.execute("INSERT INTO restaurants (name) VALUES (?)", name)
-  @restaurants = dbc.execute("SELECT name FROM restaurants")
-  erb :restaurants
+  status 201
+  { message: "Restaurant added successfully" }.to_json
 end
 
+# Récupération d'un restaurant par son ID
 get '/restaurant/:id' do
   authenticate!
   id = params[:id]
-  @restaurant = dbc.execute("SELECT * FROM restaurants WHERE id = ?", id)
-  if @restaurant.any?
-    erb :restaurant
+  restaurant = dbc.execute("SELECT * FROM restaurants WHERE id = ?", [id]).first
+  
+  if restaurant
+    { restaurant: restaurant }.to_json
   else
-    "Aucun restaurant avec cet id"
+    halt 404, { message: "Restaurant not found" }.to_json
   end
 end
 
+# Suppression d'un restaurant par son ID
 delete '/restaurant/:id' do
   authenticate!
   id = params[:id]
-  result = dbc.execute("DELETE FROM restaurants WHERE id = ?", id)
+  result = dbc.execute("DELETE FROM restaurants WHERE id = ?", [id])
+  
   if result.changes > 0
-    "Restaurant supprimé avec succès"
+    { message: "Restaurant deleted successfully" }.to_json
   else
-    "Aucun restaurant trouvé avec cet id"
+    halt 404, { message: "Restaurant not found" }.to_json
   end
 end
 
+# Mise à jour d'un restaurant par son ID
 put '/restaurant/:id' do
   authenticate!
   id = params[:id]
   name = params[:name]
-  result = dbc.execute("UPDATE restaurants SET name = ? WHERE id = ?", [name, id])
-  if result.changes > 0
-    "Restaurant modifié avec succès"
-  else
-    "Aucun restaurant trouvé avec cet id"
+
+  if name.nil? || name.empty?
+    halt 400, { message: "Name is required" }.to_json
   end
+
+  result = dbc.execute("UPDATE restaurants SET name = ? WHERE id = ?", [name, id])
+  
+  if result.changes > 0
+    { message: "Restaurant updated successfully" }.to_json
+  else
+    halt 404, { message: "Restaurant not found" }.to_json
+  end
+end
+
+# Gestion des routes inexistantes
+not_found do
+  authenticate!
+  content_type :json
+  halt 404, { message: "Route not found" }.to_json
 end
