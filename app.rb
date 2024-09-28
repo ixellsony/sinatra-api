304600eceb65ad10c08993c21ca2224344e05ebe
ixellsony <88244854+ixellsony@users.noreply.github.com>
2024-09-28 15:28:06 +0200

    Add files via upload

--- app.rb
+++ app.rb
@@ -1,23 +1,49 @@
 require 'sinatra'
 require 'sqlite3'
+require 'rack/cors'
 
-# Crée une méthode pour accéder à la base de données
-def db_connection
-  db = SQLite3::Database.new "test.db"
-  db.results_as_hash = true # Retourne les résultats sous forme de hash
+use Rack::Cors do
+  allow do
+    origins '*' 
+    resource '*', headers: :any, methods: [:get, :post, :options]
+  end
+end
+
+def dbc
+  db = SQLite3::Database.new "data.db"
+  db.results_as_hash = true 
   db
 end
 
-# Crée la table si elle n'existe pas déjà
-db_connection.execute <<-SQL
+dbc.execute <<-SQL
   CREATE TABLE IF NOT EXISTS restaurants (
     id INTEGER PRIMARY KEY,
     name TEXT
   );
 SQL
 
-# Route principale
 get '/restaurants' do
-  @restaurants = db_connection.execute("SELECT name FROM restaurants")
+  @restaurants = dbc.execute("SELECT name FROM restaurants")
+  if @restaurants.any?
+    erb :restaurants
+  else
+    "Aucun restaurant"
+  end
+end
+
+post '/restaurant' do
+  name = params[:name]
+  dbc.execute("INSERT INTO restaurants (name) VALUES (?)", name)
+  @restaurants = dbc.execute("SELECT name FROM restaurants")
   erb :restaurants
+end
+
+get '/restaurant/:id' do
+  id = params[:id]
+  @restaurant = dbc.execute("SELECT * FROM restaurants WHERE id = ?", id)
+  if @restaurant.any?
+    erb :restaurant
+  else
+    "Aucun restaurant avec cet id"
+  end
 end
